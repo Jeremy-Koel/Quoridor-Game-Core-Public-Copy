0a54fd5f7c57a43eb4e6cdc058f207a04fea1d5b
softdev <softdev@SI-203-04.harding.edu>
2019-03-24 14:49:36 -0500

    Update GameAI.cs

--- GameCore/GameAI.cs
+++ GameCore/GameAI.cs
@@ -18,6 +18,112 @@ namespace GameCore
     /// </summary>
     class MonteCarloNode : IComparable<MonteCarloNode>
     {
+        public class PriorityQueue<T> where T : IComparable<T>
+        {
+            private List<T> data;
+
+            public PriorityQueue()
+            {
+                this.data = new List<T>();
+            }
+
+            public void Enqueue(T item)
+            {
+                data.Add(item);
+                int ci = data.Count - 1; // child index; start at end
+                while (ci > 0)
+                {
+                    int pi = (ci - 1) / 2; // parent index
+                    if (data[ci].CompareTo(data[pi]) >= 0) break; // child item is larger than (or equal) parent so we're done
+                    T tmp = data[ci]; data[ci] = data[pi]; data[pi] = tmp;
+                    ci = pi;
+                }
+            }
+
+            public T Dequeue()
+            {
+                // assumes pq is not empty; up to calling code
+                int li = data.Count - 1; // last index (before removal)
+                T frontItem = data[0];   // fetch the front
+                data[0] = data[li];
+                data.RemoveAt(li);
+
+                --li; // last index (after removal)
+                int pi = 0; // parent index. start at front of pq
+                while (true)
+                {
+                    int ci = pi * 2 + 1; // left child index of parent
+                    if (ci > li) break;  // no children so done
+                    int rc = ci + 1;     // right child
+                    if (rc <= li && data[rc].CompareTo(data[ci]) < 0) // if there is a rc (ci + 1), and it is smaller than left child, use the rc instead
+                        ci = rc;
+                    if (data[pi].CompareTo(data[ci]) <= 0) break; // parent is smaller than (or equal to) smallest child so done
+                    T tmp = data[pi]; data[pi] = data[ci]; data[ci] = tmp; // swap parent and child
+                    pi = ci;
+                }
+                return frontItem;
+            }
+
+            public T Peek()
+            {
+                T frontItem = data[0];
+                return frontItem;
+            }
+
+            public int Count()
+            {
+                return data.Count;
+            }
+
+            public override string ToString()
+            {
+                string s = "";
+                for (int i = 0; i < data.Count; ++i)
+                    s += data[i].ToString() + " ";
+                s += "count = " + data.Count;
+                return s;
+            }
+
+            public bool IsConsistent()
+            {
+                // is the heap property true for all data?
+                if (data.Count == 0) return true;
+                int li = data.Count - 1; // last index
+                for (int pi = 0; pi < data.Count; ++pi) // each parent index
+                {
+                    int lci = 2 * pi + 1; // left child index
+                    int rci = 2 * pi + 2; // right child index
+
+                    if (lci <= li && data[pi].CompareTo(data[lci]) > 0) return false; // if lc exists and it's greater than parent then bad.
+                    if (rci <= li && data[pi].CompareTo(data[rci]) > 0) return false; // check the right child too.
+                }
+                return true; // passed all checks
+            } // IsConsistent
+        } // PriorityQueue
+
+       private class MoveEvaluation : IComparable<MoveEvaluation>
+        {
+            private string move;
+            private double value;
+            private int distanceFromStart;
+
+            public MoveEvaluation(string givenMove, double givenValue, int givenDistance)
+            {
+                Move = givenMove;
+                value = givenValue;
+                distanceFromStart = givenDistance;
+            }
+
+            public string Move { get => move; set => move = value; }
+            public double Value { get => value; set => this.value = value; }
+            public int DistanceFromStart { get => distanceFromStart; set => distanceFromStart = value; }
+
+            public int CompareTo(MoveEvaluation moveEvaluation)
+            {
+                return value.CompareTo(moveEvaluation.value);
+            }
+        }
+
 
         private static readonly object boardAccess = new object();
         private readonly object childrenAccess = new object();
@@ -74,6 +180,71 @@ namespace GameCore
         public double GetVisits()
         {
             return timesVisited;
+        }
+
+        private int shortestPathfinder(string move)
+        {
+            PlayerCoordinate start;
+            int goalRow = turn == 0 ? 9 : 1;
+            int goalRowForBoard = turn == 0 ? 0 : 16;
+
+            if (move.Length != 2)
+            {
+
+                WallCoordinate wallCoordinate = new WallCoordinate(move);
+
+                board[wallCoordinate.StartRow].Set(wallCoordinate.StartCol, true);
+
+                Tuple<int, int> mid = FindMidpoint(new PlayerCoordinate(wallCoordinate.StartRow, wallCoordinate.StartCol), new PlayerCoordinate(wallCoordinate.EndRow, wallCoordinate.EndCol));
+                board[mid.Item1].Set(mid.Item2, true);
+
+                board[wallCoordinate.EndRow].Set(wallCoordinate.EndCol, true);
+
+                SetPlayerMoveValues(wallCoordinate, mid);
+                start = new PlayerCoordinate(playerLocations[turn == 0 ? 0 : 1].Row, playerLocations[turn == 0 ? 0 : 1].Col);
+            }
+            else
+            {
+                start = new PlayerCoordinate(move);
+            }
+
+            PriorityQueue<MoveEvaluation> possiblePaths = new PriorityQueue<MoveEvaluation>();
+
+            for (int i = 0; i < 12; ++i)
+            {
+                string path = "a1"; // Temporary Value
+                if (true)  // Implement check to see if current move is contained within my possible moves List
+                {
+                    possiblePaths.Enqueue(new MoveEvaluation(path, HeuristicCostEstimate(new PlayerCoordinate(path), new PlayerCoordinate(path[0].ToString() + goalRow.ToString())), 1));
+                }
+            }
+
+            int shortestPath = 0;
+            MoveEvaluation nextMove;
+
+            do
+            {
+                nextMove = possiblePaths.Dequeue();
+                PlayerCoordinate current = new PlayerCoordinate(nextMove.Move);
+
+                if (current.Row == goalRowForBoard)
+                {
+                    shortestPath = nextMove.DistanceFromStart;
+                }
+
+                for (int i = 0; i < 12; ++i)
+                {
+                    string path = "a1"; // Temporary Value
+                    if (true)  // Implement check to see if current move is contained within my possible moves List
+                    {
+                        possiblePaths.Enqueue(new MoveEvaluation(path, HeuristicCostEstimate(new PlayerCoordinate(path), new PlayerCoordinate(path[0].ToString() + goalRow.ToString())), nextMove.DistanceFromStart + 1));
+                    }
+                }
+
+            } while (possiblePaths.Count() != 0 && shortestPath == 0);
+
+            return shortestPath;
+
         }
 
         //public double GetStateValue()
